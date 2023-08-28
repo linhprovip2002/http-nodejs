50e90b0d7d6de3d6d9239dc3dd3f40bde35e36ab
linhprovip2002 <dangquangnhatlinh123@gmail.com>
2023-08-28 09:49:50 +0700

    test

--- server.mjs
+++ server.mjs
@@ -1,6 +1,64 @@
-import { createServer } from 'http';
+// import { createServer } from 'http';
 
-createServer((req, res) => {
-  res.write('Hello World!');
-  res.end();
-}).listen(process.env.PORT);
+// createServer((req, res) => {
+//   res.write('Hello World!');
+//   res.end();
+// }).listen(process.env.port);
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// initialize Express.js server and save as a variable
+// so it can be referred to as `app`
+const app = express();
+
+app.use(bodyParser.json());
+let todos = []; // In-memory storage for todos
+
+// GET endpoint to fetch all todo items
+app.get("/todos", (req, res) => {
+  res.json(todos);
+});
+
+// POST endpoint to create a new todo item
+// provide `title` and optionally `completed` in the request body as JSON
+app.post("/todos", (req, res) => {
+  const todo = {
+    id: todos.length + 1,
+    title: req.body.title,
+    completed: req.body.completed || false,
+  };
+  todos.push(todo);
+  res.status(201).json(todo);
+});
+
+// PUT endpiont to update an existing todo item with the specified `id`
+// provide updated `title` and/or `completed` in the request body as JSON
+app.put("/todos/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const todo = todos.find((t) => t.id === id);
+  if (!todo) {
+    return res.status(404).json({ error: "Todo not found" });
+  }
+  todo.title = req.body.title || todo.title;
+  todo.completed = req.body.completed || todo.completed;
+  res.json(todo);
+});
+
+// DELETE endpoint to remove an existing todo item with the specified `id`
+app.delete("/todos/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = todos.findIndex((t) => t.id === id);
+  if (index === -1) {
+    return res.status(404).json({ error: "Todo not found" });
+  }
+  todos.splice(index, 1);
+  res.status(204).send();
+});
+
+// run the server on port 3000
+// for example the app can run locally at this URL: http://localhost:3000
+const PORT = process.env.port || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
